38eabdf26c30c9c403e7dbd894fd62228c0f4fe1
zenghua <zenghua@wakedata.com>
2021-06-21 21:36:07 +0800

    update code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useEffect } from 'react'
+import { registerMicroApps, start } from 'qiankun'
 
 function App() {
+  useEffect(() => {
+    registerMicroApps([
+      {
+        name: 'front', //微应用的名称，微应用之间必须确保唯一(微应用中package.json中的name)
+        entry: '//localhost:1101', //微应用的entry地址
+        container: '#subapp-container', //微应用的容器节点的选择器
+        activeRule: '/react' //微应用的激活规则
+      },
+      {
+        name: 'vue',
+        entry: '//localhost:1103',
+        container: '#subapp-container',
+        activeRule: '/vue'
+      }
+    ])
+    start({ prefetch: 'all' })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <main id="subapp-container" />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
